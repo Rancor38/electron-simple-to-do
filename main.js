c8bd5328f3ab3dd17d51f887638ebd803d52067f
Rancor38 <zak.rancor@hotmail.com>
2023-07-04 01:04:23 -0700

    Production Build

--- main.js
+++ main.js
@@ -1,55 +1,60 @@
-//Add squirrel script
+// Add squirrel script
 if (require('electron-squirrel-startup')) app.quit();
 
-//Add live reload
-// const electron = require('electron');
+const { app, BrowserWindow } = require('electron');
+const windowStateKeeper = require('electron-window-state');
+const path = require('path');
 
-// // Enable live reload for Electron too
-// require('electron-reload')(__dirname, {
-//     // Note that the path to electron may vary according to the main file
-//     electron: require(`${__dirname}/node_modules/electron`)
-// });
+function createWindow() {
+  // Load the previous window state
+  const mainWindowState = windowStateKeeper({
+    defaultWidth: 800,
+    defaultHeight: 600,
+  });
 
-// Modules to control application life and create native browser window
-const { app, BrowserWindow } = require('electron')
-const path = require('path')
-
-function createWindow () {
-  // Create the browser window.
+  // Create the browser window using the window state
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    x: mainWindowState.x,
+    y: mainWindowState.y,
+    width: mainWindowState.width,
+    height: mainWindowState.height,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
-  })
+  });
 
-  // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  // Load your Electron application's index.html or desired file
+  mainWindow.loadFile('index.html');
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  // Save the current window state when it changes
+  mainWindow.on('resize', () => {
+    mainWindowState.saveState(mainWindow);
+  });
+
+  // Open the DevTools if needed
+  // mainWindow.webContents.openDevTools();
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-  createWindow()
+  createWindow();
 
   app.on('activate', function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
-})
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
